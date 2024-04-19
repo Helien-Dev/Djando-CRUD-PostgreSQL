5754fc346a2df421582b416059d34de497aca4f8
Helien-Dev <heliendevo@gmail.com>
2024-04-19 00:05:35 -0500

    Css and tailwind added

--- Ecommerce_CRUD/settings.py
+++ Ecommerce_CRUD/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store.apps.StoreConfig',
+    'compressor',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,12 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+COMPRESS_ROOT = BASE_DIR / 'static'
+
+COMPRESS_ENABLED = True
+
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
