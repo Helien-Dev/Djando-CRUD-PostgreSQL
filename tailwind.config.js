a65ae647e90faf6f7279c2bebc1a4479e80803b9
Helien-Dev <heliendevo@gmail.com>
2024-04-19 17:45:19 -0500

    Store added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['/store/templates/*/**.{html,js}'],
+  content: ['./static/*/**.html', './store/templates/store/**.html', '/Ecommerce_CRUD/*/**.html'],
   theme: {
     extend: {}
   },
